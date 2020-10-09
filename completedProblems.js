44bcd58607bb2f6a5e55da9f67cce82a5facea3e
Spencer Attick <spencer.attick@gmail.com>
2020-10-09 08:36:04 -0700

    add arrChunk solution

--- completedProblems.js
+++ completedProblems.js
@@ -138,3 +138,33 @@ function fizzBuzz(n) {
     }
   }
 }
+
+// _________________________________________________________________________________
+// _________________________________________________________________________________
+
+
+
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+  let newArr = [];
+  let smallArr = [];
+  while (array.length) {
+    for (let i = 0; i < size; i++) {
+      if (array.length) {
+        smallArr.push(array.shift())
+      }
+    }
+    newArr.push(smallArr);
+    smallArr = [];
+  }
+ return newArr;
+}
