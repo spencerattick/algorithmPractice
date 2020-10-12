60a41125e055bb93a4e74a273f13332a47baf7e2
Spencer Attick <spencer.attick@gmail.com>
2020-10-12 14:23:59 -0700

    add capitalize solution

--- completedProblems.js
+++ completedProblems.js
@@ -212,3 +212,28 @@ function anagrams(stringA, stringB) {
   .sort()
   .join('');
 }
+
+// _________________________________________________________________________________
+// _________________________________________________________________________________
+
+// CAPITALIZE
+
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+function capitalize(str) {
+  let strArr = str.split(' ');
+  let newArr = [];
+  strArr.forEach(function(word) {
+    newArr.push(word[0].toUpperCase() + word.slice(1, word.length))
+  })
+  return newArr.join(' ');
+}
+
+module.exports = capitalize;
