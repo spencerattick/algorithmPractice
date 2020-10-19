6f97cd0cd63753609b15cce564ef10c362ae681c
Spencer Attick <spencer.attick@gmail.com>
2020-10-18 19:12:13 -0700

    add matrix solution

--- completedProblems.js
+++ completedProblems.js
@@ -344,3 +344,69 @@ function vowels(str) {
   })
   return vowelCounter;
 }
+
+//matrix
+
+// --- Directions
+// Write a function that accepts an integer N
+// and returns a NxN spiral matrix.
+// --- Examples
+//   matrix(2)
+//     [[1, 2],
+//     [4, 3]]
+//   matrix(3)
+//     [[1, 2, 3],
+//     [8, 9, 4],
+//     [7, 6, 5]]
+//  matrix(4)
+//     [[1,  2,  3, 4],
+//     [12, 13, 14, 5],
+//     [11, 16, 15, 6],
+//     [10,  9,  8, 7]]
+
+function matrix(n) {
+  let largeArr = [];
+  let incrementor = 1;
+  let rowStart = 0;
+  let rowEnd = n - 1;
+  let colStart = 0;
+  let colEnd = n - 1;
+
+  for (let i = 0; i < n; i++) {
+    largeArr.push([]);
+  }
+
+  while (colStart <= colEnd && rowStart <= rowEnd) {
+
+  //row
+    for (let i = colStart; i <= colEnd; i++) {
+        largeArr[rowStart][i] = incrementor;
+        incrementor++;
+      }
+    rowStart++;
+
+  //col
+    for (let i = rowStart; i <= rowEnd; i++) {
+        largeArr[i][colEnd] = incrementor;
+        incrementor++;
+      }
+    colEnd--;
+
+  //row
+    for (let i = colEnd; i >= colStart; i--) {
+      largeArr[rowEnd][i] = incrementor;
+      incrementor++;
+    }
+    rowEnd--;
+
+  //col
+    for (let i = rowEnd; i >= rowStart; i--) {
+        largeArr[i][colStart] = incrementor;
+      incrementor++;
+    }
+    colStart++;
+
+  }
+
+  return largeArr;
+}
