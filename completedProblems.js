df74a46ed467524302dac180d3b639febb31d3a8
Spencer Attick <spencer.attick@gmail.com>
2020-10-20 16:07:50 -0700

    implement queue

--- completedProblems.js
+++ completedProblems.js
@@ -442,3 +442,32 @@ function fib(n) {
   }
   return fibonacci[n];
 }
+
+// _________________________________________________________________________________
+// _________________________________________________________________________________
+
+// QUEUE IMPLEMENTATION
+
+// --- Description
+// Create a queue data structure.  The queue
+// should be a class with methods 'add' and 'remove'.
+// Adding to the queue should store an element until
+// it is removed
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.remove(); // returns 1;
+
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+
+  add(item) {
+    this.queue.unshift(item);
+  }
+
+  remove() {
+    return this.queue.pop();
+  }
+}
